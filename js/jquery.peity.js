8b0be2991f3d5ca063858db3b939d47ddadee1ab
Pborn <87150258+pborn84@users.noreply.github.com>
2022-10-26 03:10:06 -0300

    Init - project
    Inicio do site

---
+++ js/jquery.peity.js
@@ -0,0 +1,20 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var chart = window.chart = new EasyPieChart(document.querySelector('span'), {
+      easing: 'easeOutElastic',
+      delay: 3000,
+      barColor: '#69c',
+      trackColor: '#ace',
+      scaleColor: false,
+      lineWidth: 20,
+      trackWidth: 16,
+      lineCap: 'butt',
+      onStep: function(from, to, percent) {
+        this.el.children[0].innerHTML = Math.round(percent);
+      }
+    });
+
+    document.querySelector('.js_update').addEventListener('click', function(e) {
+      chart.update(Math.random()*200-100);
+    });
+
+  });
